43ff7683ad52fe3813c0a24939fc25ea21abbc96
Julian Chukwu <jcchuks@ymail.com>
2017-04-11 16:37:14 -0400

    Power of Two from Leetcode

---
+++ PowerOfTwo.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n < 1:
+            return False
+        
+        while n != 1:
+            if n & 1:
+                return False
+            n = n >> 1
+            
+        return True
+            
